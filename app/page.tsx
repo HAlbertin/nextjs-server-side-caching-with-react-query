db4e9cb0e326b52ba2485535e76998a1b0f81b8a
Hebert Albertin <hebert_albertin@hotmail.com>
2026-04-06 23:10:01 +0100

    fix: suspense for search-params

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Pokemons } from "./_components/Pokemons";
 import { prefetchPokemonTypes } from "./_services/prefetch-pokemon-types";
+import { Suspense } from "react";
+import { Loading } from "./_components/Loading";
 
 export default function Home() {
   const queryClient = prefetchPokemonTypes();
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Pokemons />
+      <Suspense fallback={<Loading />}>
+        <Pokemons />
+      </Suspense>
     </HydrationBoundary>
   );
 }
